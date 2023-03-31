a738fe4de3d948b9104466711fb134058fcf349c
Alexander <bulgakov453@gmail.com>
2023-03-31 18:32:00 +0500

    консольное приложение

---
+++ app/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] words;
+Console.WriteLine("Введите слова через запятую: ");
+string row = Console.ReadLine();
+words = row.Split(',');
+var podschet = new string[words.Length];
+var dl = 0;
+foreach (var value in words)
+{
+    if (value.Length <= 3)
+    {
+        podschet[dl] = value;
+        dl++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, podschet, 0, dl));
+Console.ReadKey(true); 
